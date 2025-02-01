4ecd2ee45f644baa4400f4d2a8a6eb34886a49b1
Tolevats <142176061+Tolevats@users.noreply.github.com>
2025-02-01 17:17:28 -0300

    feat: Configure Vuetify

--- my-portfolio/src/main.ts
+++ my-portfolio/src/main.ts
@@ -5,10 +5,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(createVuetify())
 
 app.mount('#app')
