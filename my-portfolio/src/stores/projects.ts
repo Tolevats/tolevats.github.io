8ddcc4537cf0007c259ab5bb13a3662785cd08d3
Tolevats <142176061+Tolevats@users.noreply.github.com>
2025-04-17 21:52:15 -0400

    update: Add  project titles and descriptions in projects store

--- my-portfolio/src/stores/projects.ts
+++ my-portfolio/src/stores/projects.ts
@@ -4,7 +4,9 @@ export const useProjectsStore = defineStore('projects', {
   state: () => ({
     projects: [
       { id: 1, title: 'Portfolio', description: 'My portfolio in Vue.js' },
-      { id: 2, title: 'Notes', description: 'A simple app to create and edit notes' },
+      { id: 2, title: 'GoEco!', description: 'Your eco-friendly event planner' },
+      { id: 3, title: 'OnPointStore', description: 'An ecommerce platform prototype' },
+      { id: 4, title: 'Note App', description: 'A simple app to handle your notes' },
     ],
   }),
 })
